37b5f0d7919110e5ae083e7e22b10f559e01e189
kalleby santos <kalleby_santos@hotmail.com>
2022-05-21 15:56:24 +0100

    📦 NEW: Serving SPA Static Files

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddSpaStaticFiles(config =>
+        config.RootPath = "Client/dist"
+    );
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSpaStaticFiles();
+app.UseSpa(spaBuilder => 
+{
+    spaBuilder.Options.SourcePath = "Client";
+    
+    Console.WriteLine(builder.Environment.IsDevelopment());
+    Console.WriteLine(app.Environment.IsDevelopment());
+
+    if(builder.Environment.IsDevelopment())
+    {
+        spaBuilder.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+    }
+});
+
+app.UseRouting();
 
 app.Run();
