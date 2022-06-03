39d82b9954804eb5d6f80027a44b6e5ad88f7904
kalleby santos <kalleby_santos@hotmail.com>
2022-06-03 17:47:45 +0100

    👌 IMPROVE: Moving upload logic to dedicated extension methods

---
+++ App/Domains/Files/Extensions/FileServerExtension.cs
@@ -0,0 +1,35 @@
+using tusdotnet;
+using tusdotnet.Stores;
+using tusdotnet.Models;
+using tusdotnet.Interfaces;
+using tusdotnet.Models.Configuration;
+
+namespace CloudIn.Domains.Files.Extensions;
+
+public static class FileServerExtension
+{
+    public static IApplicationBuilder UseFileHandler(this IApplicationBuilder app) =>
+        app.UseTus(httpContext => new DefaultTusConfiguration
+        {
+            Store = new TusDiskStore(@"./Public/Cache"),
+            UrlPath = "/files",
+            Events = new Events
+            {
+                OnFileCompleteAsync = async eventContext =>
+                {
+                    // eventContext.FileId is the id of the file that was uploaded.
+                    // eventContext.Store is the data store that was used (in this case an instance of the TusDiskStore)
+
+                    // A normal use case here would be to read the file and do some processing on it.
+                    ITusFile file = await eventContext.GetFileAsync();
+                    Console.WriteLine($"Upload of {eventContext.FileId} completed using {eventContext.Store.GetType().FullName}");
+
+                    if (file == null)
+                    {
+                        return;
+                    }
+
+                }
+            }
+        });
+}
--- Program.cs
+++ Program.cs
@@ -4,13 +4,7 @@ using CloudIn.Domains.Data.Extensions;
 using CloudIn.Domains.GraphQl;
 using CloudIn.Contexts.Files;
 using CloudIn.Contexts.Folders;
-using tusdotnet;
-using tusdotnet.Models;
-using tusdotnet.Stores;
-using tusdotnet.Models.Configuration;
-using Microsoft.AspNetCore.Http.Features;
-using tusdotnet.Interfaces;
-using System.Text;
+using CloudIn.Domains.Files.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,34 +28,7 @@ builder.Services
 
 var app = builder.Build();
 
-app.UseTus(httpContext => new DefaultTusConfiguration
-{
-    Store = new TusDiskStore(@"./Public/Uploads"),
-    UrlPath = "/files",
-    Events = new Events
-    {
-        OnFileCompleteAsync = async eventContext =>
-        {
-            // eventContext.FileId is the id of the file that was uploaded.
-            // eventContext.Store is the data store that was used (in this case an instance of the TusDiskStore)
-
-            // A normal use case here would be to read the file and do some processing on it.
-            ITusFile file = await eventContext.GetFileAsync();
-            Console.WriteLine($"Upload of {eventContext.FileId} completed using {eventContext.Store.GetType().FullName}");
-
-            if (file == null)
-            {
-                return;
-            }
-
-            var result = await DoSomeProcessing(file, eventContext.CancellationToken).ConfigureAwait(false);
-            if (!result)
-            {
-                //throw new MyProcessingException("Something went wrong during processing");
-            }
-        }
-    }
-});
+app.UseFileHandler();
 
 app.UseRouting();
 app.UseEndpoints(config =>
@@ -87,30 +54,3 @@ if (builder.Environment.IsDevelopment())
 
 app.Run();
 
-async Task<bool> DoSomeProcessing(ITusFile file, CancellationToken cancellationToken)
-{
-    var metadata = await file.GetMetadataAsync(cancellationToken);
-
-    var fileType = metadata["filetype"].GetString(Encoding.UTF8);
-
-    var fileExtension = "";
-
-    if (fileType == "image/png")
-    {
-        fileExtension = ".png";
-    }
-
-    var fileContent = await file.GetContentAsync(cancellationToken);
-
-    var fileName = $"file{fileExtension}";
-    var folderPath = $"./Public/{file.Id}";
-
-    Directory.CreateDirectory(folderPath);
-
-    using (var fileStream = new FileStream($"{folderPath}/{fileName}", FileMode.CreateNew, FileAccess.Write))
-    {
-        await fileContent.CopyToAsync(fileStream);
-    }
-
-    return await Task.FromResult(true);
-}
